c9d08d6570d2b62ad1574d235f95ebae73390f53
Jude Taylor <me@jude.xyz>
2019-09-11 15:55:03 -0700

    test with 8.8

--- shell.nix
+++ shell.nix
@@ -2,4 +2,6 @@ with import <nixpkgs> {};
 
 haskellPackages.developPackage {
   root = ./.;
+  # needed for cabal-helper, which isn't 3.0 ready
+  modifier = drv: haskell.lib.addSetupDepend drv haskellPackages.Cabal_2_4_1_0;
 }
