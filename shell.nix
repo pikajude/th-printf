6c34049ae546c24b2ab58861ed65c229a3865fe4
Jude Taylor <me@jude.xyz>
2019-09-12 09:43:49 -0700

    add a comment re: hpack

--- shell.nix
+++ shell.nix
@@ -11,6 +11,7 @@ in hp.developPackage {
   # needed for cabal-helper, which isn't 3.0 ready
   modifier = drv: haskell.lib.addSetupDepend drv hp.Cabal_2_4_1_0;
   overrides = _: _: {
+    # don't auto-run hpack. it has a ton of dependencies and I already run it manually
     hpack = null;
   };
 }
